dbe686bf52ae3b132d178e663744486175158e1d
massiv1984 <emil.kero@elev.ga.ntig.se>
2023-10-19 10:55:29 +0200

    steg 7 färdig yo

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,4 @@
+import Slime from './Slime.js'
 import Player from './Player.js'
 import InputHandler from './InputHandler.js'
 import UserInterface from './UserInterface.js'
@@ -14,6 +15,9 @@ export default class Game {
     this.gravity = 1
     this.debug = false
     this.player = new Player(this)
+    this.enemies = []
+    this.enemyTimer = 0
+    this.enemyInterval = 0.000000000001
   }
 
   update(deltaTime) {
@@ -21,10 +25,25 @@ export default class Game {
       this.gameTime += deltaTime
     }
     this.player.update(deltaTime)
+
+    if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+      this.addEnemy()
+      this.enemyTimer = 0
+    } else {
+      this.enemyTimer += deltaTime
+    }
+
+    this.enemies.forEach((enemy) => enemy.update(deltaTime))
+    this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion)
   }
 
   draw(context) {
     this.player.draw(context)
     this.ui.draw(context)
+    this.enemies.forEach((enemy) => enemy.draw(context))
+  }
+
+  addEnemy() {
+    this.enemies.push(new Slime(this))
   }
 }
